e1ca383d42587b5e6215ad1cdc4ddbf1b38a23a5
Hedvighedvig <hedvig.reese@hotmail.com>
2021-11-24 11:45:06 +0100

    My first commit!

---
+++ psychedelicscript.js
@@ -0,0 +1,22 @@
+function modalBox(){
+  document.addEventListener("click", function (event) {
+  ;
+      const target = event.target;
+      if (target.hasAttribute('data-box')){
+          if (target.hasAttribute('data-window')){
+              const dynamicID = target.getAttribute('data-window');
+              document.getElementById(dynamicID).classList.add('open-modal');
+          }
+      }
+
+      if (target.hasAttribute('data-dismiss') || target.classList.contains('modal')){
+          const modal = document.querySelector('[class="modal open-modal"]');
+          modal.classList.remove('open-modal');
+      }
+
+  });
+}
+modalBox();
+
+var audio = new Audio('psychedelicclock.mp3');
+audio.play();
